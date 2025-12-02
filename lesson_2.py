1a8c651bdd6279ccda5a8712215c89eba8aa7a1b
Igor <igglike@gmail.com>
2025-12-02 18:52:54 +0600

    Добавил примеры с наследованием и полим-м

---
+++ lesson_2.py
@@ -0,0 +1,48 @@
+# родительский класс, суперкласс
+class Car:
+    # инициализатор(конструктор)
+    def __init__(self, color, model):
+        self.color = color
+        self.model = model
+        self.fined = False
+
+    def drive_to(self, destination):
+        print(f"Car {self.model} driving to {destination}")
+
+    def change_color(self, new_color):
+        self.color = new_color
+        print(f"Car {self.model} changed to {self.color}")
+
+
+# дочерний класс, подкласс
+class Bus(Car):
+    def __init__(self, color, model, number):
+        super().__init__(color, model)
+        self.number = number
+
+
+class Truck(Car):
+    def drive_to(self, destination):
+        # super().drive_to(destination)
+        print(f"Truck {self.model} driving to {destination}")
+
+    def test(self):
+        print(f"Truck test: {self.model}")
+
+
+
+car_honda = Car(color = "red", model = "Honda")
+car_subaru = Car(color = "blue", model = "Subaru")
+car_subaru.drive_to("Karakol")
+truck_1 = Truck("red", "Mercedes")
+truck_1.drive_to("Bishkek")
+print("Truck 1:", truck_1.color, truck_1.model)
+truck_1.test()
+
+bus_42 = Bus(color = "red", model = "Honda", number = 42)
+print("Bus 42:", bus_42.color, bus_42.model, bus_42.number)
+
+
+vehicles = [car_honda, truck_1, bus_42]
+for v in vehicles:
+    v.drive_to(destination="Karakol")
